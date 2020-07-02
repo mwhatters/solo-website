370cf9bdc5260a814d442e780fb82a91f11f2e95
Marshall Hattersley <marshallhattersley@Marshalls-MacBook-Air.local>
2020-07-02 09:38:37 -0700

    fix jsx error

--- src/lib/profileContent.js
+++ src/lib/profileContent.js
@@ -8,7 +8,7 @@ export const profileContent = (
       web development, and philosophy. I've had years of experience
         in the software world working for <a href="https://www.intercom.com/">Intercom</a> but
         recently took time away to pursue composition and video game music development.
-      </p>
+    </p>
     <p>
       Since leaving Intercom in March 2019, I've taken the time to build audio engineering skills and learn a little bit more about audio game engines. 
       During this hiatus, I've also composed over 2 hours worth of original music, done extensive
@@ -19,18 +19,19 @@ export const profileContent = (
       I just moved down to San Diego, California, and in search for work!
       I have web development (ruby on rails, react, ember), music composition, and dynamic audio engineering experiennce.
       At this point I'm looking for work in any of those fields, though I admittedly have the most professional experience in software.
-    <p>
+    </p>
     <p>
     In an effort to de-rust on React, I made this website. Consider this site a library of all of the projects I've done over the last few years.
     I'll continue to update it as I produce more work!
     I built this using the react framework Gatsby.
     </p>
+    <p>
       <a
         href="https://drive.google.com/file/d/1qO16UjgJumDRsGEuqUTzHhTJZHNru0aQ/view?usp=sharing"
         target="_blank"
       >
         Link to my CV
-        </a>
+      </a>
     </p>
   </div>
 )
