6d4c5dd8de755bf5f01ae779043160e28f2d0e51
Marshall Hattersley <mwhatters@gmail.com>
2021-12-20 15:49:29 -0800

    update config

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,7 +31,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
+        name: `mwhatt-solo-website`,
         short_name: `starter`,
         start_url: `/`,
         background_color: `#663399`,
